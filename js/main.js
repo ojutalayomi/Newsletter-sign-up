c1756183c228a725f53bd83c8ebf22165a96e310
Ayomide Ojutalayo <ojutalayoayomide21@gmail.com>
2023-12-04 11:30:54 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-(function ($) {
-    "use strict";
-    
-    // loader
-    var loader = function () {
-        setTimeout(function () {
-            if ($('#loader').length > 0) {
-                $('#loader').removeClass('show');
-            }
-        }, 1000);
-    };
-    loader();
-})(jQuery);
+
+// Import the necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const validator = require('validator');
+
+// Create an Express application
+const app = express();
+
+// Use body-parser to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define a route to handle form submissions
+app.post('/submit', (req, res) => {
+    // Access form data
+    const email = req.body.email;
+
+    // Validate the email address
+    if (validator.isEmail(email)) {
+        // If the email is valid, do something with it (like save it in a database)
+        console.log(`Received a valid email: ${email}`);
+        res.send('Form submission received');
+    } else {
+        // If the email is not valid, send an error message
+        console.log(`Received an invalid email: ${email}`);
+        res.status(400).send('Invalid email address');
+    }
+});
+
+// Start the server
+app.listen(5500, () => console.log('Server is listening on port 5500'));
+
